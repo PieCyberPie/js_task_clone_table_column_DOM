cab423cf11ed94b7880333ff4045795196830d52
Daniil <piecyberpie@gmail.com>
2024-06-26 19:03:15 +0300

    make build

---
+++ index.36a32c05.js
@@ -0,0 +1,2 @@
+var r=document.querySelectorAll("tr"),e=!0,l=!1,n=void 0;try{for(var t,o=r[Symbol.iterator]();!(e=(t=o.next()).done);e=!0){var a=t.value,i=a.querySelectorAll("td");0===i.length&&(i=a.querySelectorAll("th"));var u=i[4].cloneNode(!0);i[4].innerHTML=i[3].innerHTML,i[4].innerHTML=i[1].innerHTML,a.appendChild(u)}}catch(r){l=!0,n=r}finally{try{e||null==o.return||o.return()}finally{if(l)throw n}}
+//# sourceMappingURL=index.36a32c05.js.map
